937da03fe342aa2987d321a5a570810256b2367f
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-04-15 20:13:31 -0300

    Feat(Main): Add Main sample code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Birthday birthday = new Birthday(5);
+        int candlesBlowed;
+        birthday.addCandle(2);
+        birthday.addCandle(5);
+        birthday.addCandle(6);
+        birthday.addCandle(6);
+        birthday.addCandle(3);
+        candlesBlowed =  birthday.blowCandles();
+        System.out.println("For her " + birthday.getAge() + "th birthday, she got to blow " + candlesBlowed + " candles!!");
     }
 }
