70f2778d70cd8764baac4c191267004fcd1dde95
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-04-15 15:06:50 -0300

    test(BdayTest-Class): add shouldBlowOutEveryCandle test

---
+++ src/test/java/BirthdayTest.java
@@ -0,0 +1,24 @@
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class BirthdayTest {
+    @Test
+    @DisplayName("test")
+    void test(){
+        System.out.printf("fail");
+    }
+
+    @Test
+    @DisplayName("Should blow out every candle")
+    void shouldBlowOutEveryCandle(){
+        Birthday birthday = new Birthday(3);
+        birthday.addCandle(2);
+        birthday.addCandle(2);
+        birthday.addCandle(2);
+        assertThat(birthday.blowCandles).isEqualTo(3);
+    }
+}
